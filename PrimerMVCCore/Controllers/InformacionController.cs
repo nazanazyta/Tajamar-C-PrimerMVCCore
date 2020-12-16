8858d2804517401ef6dd2c8e3718d883d2337268
Nazaret <nazari_93@hotmail.com>
2020-12-16 15:13:51 +0100

    clase casa 16/12/2020

---
+++ PrimerMVCCore/Controllers/InformacionController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PrimerMVCCore.Models;
+
+namespace PrimerMVCCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        //GET VistaDatos
+        public IActionResult VistaDatos(String aplicacion, int version)
+        {
+            //LOS PARÁMETROS VAN ENLAZADOS POR SU name
+            //Y SON OPCIONALES
+            ViewBag.Aplicacion = aplicacion;
+            ViewBag.Version = version;
+            return View();
+        }
+
+        //POST
+        public IActionResult VistaPost()
+        {
+            return View();
+        }
+
+        //PARA CAPTURAR LA INFORMACIÓN DE UN FORMULARIO
+        //NECESITAMOS UN MÉTODO EXTRA POST
+        //DICHO MÉTODO DEBE TENER EL ATRIBUTO [HttpPost]
+        //RECIBE LOS PARÁMETROS CON EL MISMO name
+        //QUE LAS CAJAS O INPUT
+        //PARA QUE SEA FUNCIONAL TIENE QUE TENER [HttpPost]
+        [HttpPost]
+        public IActionResult VistaPost(String cajanombre, String cajaedad)
+        {
+            ViewBag.Nombre = cajanombre;
+            ViewBag.Edad = cajaedad;
+            return View();
+        }
+
+        public IActionResult Inicio()
+        {
+            //ViewBag.Nombre = "Usuario";
+            ViewBag.Edad = 27;
+            ViewData["nombre"] = "Naza";
+            //ViewData["edad"] = 29;
+            Persona person = new Persona();
+            person.Nombre = "Soy un modelo";
+            person.Edad = 99;
+            person.Email = "email@email.com";
+            //TODOS LOS MÉTODOS DEBEN DEVOLVER UNA VISTA
+            return View(person);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrimerMVCCore/Models/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerMVCCore.Models
+{
+    public class Persona
+    {
+        public String Nombre { get; set; }
+        public int Edad { get; set; }
+        public String Email { get; set; }
+    }
+}
